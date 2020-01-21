d5a26ec6b96792f1e0f88d63a56dbffbf486db04
unknown <nexog1997@gmail.com>
2020-01-21 23:05:04 +0100

    added navigator

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack'
+import {createAppContainer} from 'react-navigation'
+import SearchScreen from './src/screens/SearchScreen'
+const navigator = createStackNavigator({
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up A.js to start woss on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  Search:SearchScreen
+
+  
+
+},{initialRouteName:'Search',
+defaultNavigationOptions:{
+  title:'Food app!'
+}})
+
+export default createAppContainer(navigator)
